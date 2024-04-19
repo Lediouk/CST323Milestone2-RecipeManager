de1dacd838c0fd512b2ec2a9495c3d6a5e33ac34
Dioukou <126500344+Lediouk@users.noreply.github.com>
2024-04-18 22:33:48 -0700

    Update RecipeController.java

--- src/main/java/com/recipemanagement/controller/RecipeController.java
+++ src/main/java/com/recipemanagement/controller/RecipeController.java
@@ -3,10 +3,16 @@ package com.recipemanagement.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.recipeapp.entity.Recipe;
-import com.example.recipeapp.service.RecipeService;
+import com.recipemanagement.entity.Recipe;
+import com.recipemanagement.service.RecipeService;
 
 @Controller
 @RequestMapping("/recipes")
