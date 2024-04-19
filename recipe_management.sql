d5b7342ab61f72153038941d03959e93ee5d1c55
Dioukou <126500344+Lediouk@users.noreply.github.com>
2024-04-18 21:44:59 -0700

    Add files via upload

---
+++ recipe_management.sql
@@ -0,0 +1,88 @@
+-
+-- Database: `recipe_management`
+--
+CREATE DATABASE IF NOT EXISTS `recipe_management` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `recipe_management`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipes`
+--
+
+CREATE TABLE `recipes` (
+  `RecipeID` int(11) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Category` varchar(50) DEFAULT NULL,
+  `Ingredients` text,
+  `Instructions` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `recipes`
+--
+
+INSERT INTO `recipes` (`RecipeID`, `Name`, `Description`, `Category`, `Ingredients`, `Instructions`) VALUES
+(1, 'Spaghetti Carbonara', 'Classic Italian pasta dish with creamy sauce', 'Pasta', 'Spaghetti, eggs, bacon, Parmesan cheese, black pepper', '1. Cook spaghetti according to package instructions. ...'),
+(2, 'Grilled Chicken Salad', 'Healthy salad with grilled chicken and fresh veggies', 'Salad', 'Chicken breast, lettuce, tomato, cucumber, bell pepper', '1. Season chicken breast with salt and pepper. 2. ...'),
+(3, 'Chocolate Chip Cookies', 'Classic homemade cookies with chocolate chips', 'Dessert', 'Butter, sugar, brown sugar, eggs, vanilla extract, flour, baking soda, salt, chocolate chips', '1. Preheat oven to 350°F (175°C). 2. In a mixing b...');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(50) DEFAULT NULL,
+  `Password` varchar(50) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `Username`, `Password`, `Email`) VALUES
+(1, 'john_doe', 'password123', 'john@example.com'),
+(2, 'jane_smith', 'letmein', 'jane@example.com'),
+(3, 'bob_jones', 'securepassword', 'bob@example.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `recipes`
+--
+ALTER TABLE `recipes`
+  ADD PRIMARY KEY (`RecipeID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `recipes`
+--
+ALTER TABLE `recipes`
+  MODIFY `RecipeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
